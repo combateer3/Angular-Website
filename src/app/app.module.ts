8963e46391a2385601b2702befbc81118c49835e
combateer3 <scottcorbat@yahoo.com>
2020-08-11 00:22:35 -0400

    Redesigned front page
    New nav bar
    Small intro header with animated subtitle
    Goal is to use as much custom CSS as possible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -10,11 +9,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { NavComponent } from './nav/nav.component';
+import { IAmAComponent } from './i-am-a/i-am-a.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    IAmAComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/i-am-a/i-am-a.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
+@Component({
+  selector: 'app-i-am-a',
+  animations: [
+    trigger('displayName', [
+      state('in', style({opacity: 1})),
+      state('out', style({opacity: 0, display: 'none'})),
+      transition('in => out', [
+        animate('0.4s')
+      ]),
+      transition('out => in', [
+        animate('0.4s')
+      ])
+    ])
+  ],
+  templateUrl: './i-am-a.component.html',
+  styleUrls: ['./i-am-a.component.css']
+})
+export class IAmAComponent implements OnInit {
+
+  current = 0;
+
+  items = [
+    {
+      value: 'Michigan Tech student',
+      animationState: 'in'
+    },
+    {
+      value: 'web developer',
+      animationState: 'out'
+    },
+    {
+      value: 'android developer',
+      animationState: 'out'
+    }
+    ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval((function(): void {
+      this.items[this.current].animationState = 'out';
+      this.current++;
+      if (this.current >= this.items.length) {
+        this.current = 0;
+      }
+      this.items[this.current].animationState = 'in';
+    }).bind(this), 4000);
+  }
+
+}
